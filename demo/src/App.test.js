b11682bcd4b7ed721aabd1a8f02f00ef9f36769c
Michael Nash <michael@spatialnetworks.com>
2019-12-02 14:16:04 -0500

    setting up enzyme

--- demo/src/App.test.js
+++ demo/src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import Enzyme from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
+import App from './App'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render( < App / > , div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+	const div = document.createElement('div')
+})
