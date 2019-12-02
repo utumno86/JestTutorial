55d01c522c5e027a77d5269c08765e2a06de3999
Michael Nash <michael@spatialnetworks.com>
2019-12-02 14:42:16 -0500

    experimenting with enzyme

--- demo/src/App.test.js
+++ demo/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Enzyme from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 
 import App from './App'
@@ -7,5 +7,6 @@ import App from './App'
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 test('renders without crashing', () => {
-	const div = document.createElement('div')
+	const wrapper = shallow(<App />)
+	expect(wrapper).toBeTruthy()
 })
