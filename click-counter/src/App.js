a1635149e03b378a4a6ad9459ef638071f2a63d3
Michael Nash <michael@spatialnetworks.com>
2019-12-05 12:14:56 -0500

    actually implemented that

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -6,16 +6,30 @@ class App extends Component {
 		super(props)
 		this.state = {
 			counter: 0,
+			error: false,
 		}
 	}
+
+	decrement = () => {
+		if (this.state.counter > 0) this.setState({ counter: this.state.counter - 1 })
+		else this.setState({ error: true })
+	}
+
+	getErrorDisplay = () => {
+		if (this.state.error) {
+			return <h3 data-test="error-display"> Cannot decrement lower than 0 !</h3>
+		}
+	}
+
 	render() {
 		return (
 			<div data-test="component-app">
 				<h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+				{this.getErrorDisplay()}
 				<button data-test="increment-button" onClick={() => this.setState({ counter: this.state.counter + 1 })}>
 					Increment!
 				</button>
-				<button data-test="decrement-button" onClick={() => this.setState({ counter: this.state.counter - 1 })}>
+				<button data-test="decrement-button" onClick={this.decrement}>
 					Decrement!
 				</button>
 			</div>
