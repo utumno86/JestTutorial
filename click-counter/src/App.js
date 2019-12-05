feb38b1976a7571250639d0a4e44b959d8b58715
Michael Nash <michael@spatialnetworks.com>
2019-12-05 12:49:41 -0500

    implemented error removal

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -26,7 +26,10 @@ class App extends Component {
 			<div data-test="component-app">
 				<h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
 				{this.getErrorDisplay()}
-				<button data-test="increment-button" onClick={() => this.setState({ counter: this.state.counter + 1 })}>
+				<button
+					data-test="increment-button"
+					onClick={() => this.setState({ counter: this.state.counter + 1, error: false })}
+				>
 					Increment!
 				</button>
 				<button data-test="decrement-button" onClick={this.decrement}>
