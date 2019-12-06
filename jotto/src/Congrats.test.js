a0ed2189edc2119f575cbbf25f889a9c3aed249a
Michael Nash <michael@spatialnetworks.com>
2019-12-06 16:14:30 -0500

    testing out the Congrats component

---
+++ jotto/src/Congrats.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
+import Congrats from './Congrats'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+/**
+ *Factory function to create a shallow wrapper for the Congrats component
+ * @function setup
+ * @param {object} props
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+	return shallow(<Congrats {...props} />)
+}
+
+test('renders without error', () => {})
+
+test('renders no text when success prop is false', () => {})
+
+test('renders non-empty congrats message when success prop is true', () => {})
