69fb4f0dc12fcffc03c656f87f87e4f4865a2006
Michael Nash <michael@spatialnetworks.com>
2019-12-06 17:12:46 -0500

    wrote out the bare minimum code to make the tests pass

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -3,8 +3,17 @@ import React from 'react'
 /**
  * Functional react component for congratulatory message.
  * @function
+ * @param {object} props - React props
  * @returns {JSX.Element} - Rendered component
  */
-export default () => {
-	return <div></div>
+export default props => {
+	if (props.success) {
+		return (
+			<div data-test="component">
+				<span data-test="congrats-message">Congratulations! You guessed the word!</span>
+			</div>
+		)
+	} else {
+		return <div data-test="component-congrats" />
+	}
 }
--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -30,7 +30,7 @@ test('renders no text when success prop is false', () => {
 })
 
 test('renders non-empty congrats message when success prop is true', () => {
-	const wrapper = setup({ success: false })
+	const wrapper = setup({ success: true })
 	const message = findByTestAttr(wrapper, 'congrats-message')
 	expect(message.text().length).not.toBe(0)
 })
