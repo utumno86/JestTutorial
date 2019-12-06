579807f74ee5a4e6e571a06be20b072571590994
Michael Nash <michael@spatialnetworks.com>
2019-12-06 16:27:34 -0500

    get render tests working

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 
+import { findByTestAttr } from '../test/testUtils'
 import Congrats from './Congrats'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
@@ -16,7 +17,11 @@ const setup = (props = {}) => {
 	return shallow(<Congrats {...props} />)
 }
 
-test('renders without error', () => {})
+test('renders without error', () => {
+	const wrapper = setup()
+	const component = findByTestAttr(wrapper, 'component-congrats')
+	expect(component.length).toBe(1)
+})
 
 test('renders no text when success prop is false', () => {})
 
