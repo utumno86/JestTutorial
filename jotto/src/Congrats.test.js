15a1e7a10b031beda90f785898fd10bd741f7dc5
Michael Nash <utumno86@gmail.com>
2020-02-17 16:06:20 -0500

    refactored enzyme setup into setup file

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16'
+import { shallow } from 'enzyme'
 
 import { findByTestAttr, checkProps } from '../test/testUtils'
 import Congrats from './Congrats'
-
-Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 /**
  *Factory function to create a shallow wrapper for the Congrats component
