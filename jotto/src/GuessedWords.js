2b90f05a71e996cebdb2d929db74f94c8fa4d2a9
Michael Nash <utumno86@gmail.com>
2020-02-19 13:24:47 -0500

    coded guessed words table

--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -9,6 +9,37 @@ const GuessedWords = (props) => {
         Try to guess the word!
       </span>
     )
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, index) => (
+      <tr key={index} data-test='guessed-word'>
+        <td>
+          {word.guessedWord}
+        </td>
+        <td>
+          {word.letterMatchCount}
+        </td>
+      </tr>
+    ))
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guesed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                Guess
+              </th>
+              <th>
+                Matching Letter
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            { guessedWordsRows }
+          </tbody>
+        </table>
+      </div>
+    )
   }
   return(
     <div data-test="component-guessed-words">
@@ -20,7 +51,7 @@ const GuessedWords = (props) => {
 GuessedWords.propTypes = {
   guessedWords: PropTypes.arrayOf(
     PropTypes.shape({
-      guessedWords: PropTypes.string.isRequired,
+      guessedWord: PropTypes.string.isRequired,
       letterMatchCount: PropTypes.number.isRequired
     })
   ).isRequired
