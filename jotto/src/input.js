9c0f57d50913e6e6f84f63afb2e3ff71d132882b
Michael Nash <utumno86@gmail.com>
2020-03-19 15:49:35 -0400

    complete input component

--- jotto/src/input.js
+++ jotto/src/input.js
@@ -3,16 +3,23 @@ import { connect } from 'react-redux'
 
 class Input extends Component {
   render() {
-    return (
+    const contents = this.props.success ? null :
+    (
+      <form className='form-inline'>
+        <input data-test='input-box' className='mb-2 mx-sm-3' type='text' placeholder='enter guess'/>
+        <button data-test='submit-button' className='btn btn-primary mb-2' type='submit'>Submit</button> 
+      </form>
+    )
+    return(
       <div>
-        <button></button>
+        {contents}
       </div>
     )
   }
 }
 
-const mapStateToProps= (state) => {
-  return {}
+const mapStateToProps= ({ success }) => {
+  return { success }
 }
 
 export default connect(mapStateToProps)(Input)
--- jotto/src/input.test.js
+++ jotto/src/input.test.js
@@ -14,14 +14,22 @@ setup()
 
 describe('render', () => {
   describe('word has not been guessed', () =>{
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false }
+      wrapper = setup(initialState)
+    })
     test('renders component without error', () => {
-
+      const component = findByTestAttr(wrapper, 'component-input')
+      expect(component.length).toBe(1)
     })
     test('renders input box', () => {
-
+      const inputBox = findByTestAttr(wrapper, 'input-box')
+      expect(inputBox.length).toBe(1)
     })
     test('renders submit button', () => {
-
+      const submitButton = findByTestAttr(wrapper, 'component-input')
+      expect(submitButton.length).toBe(1)
     })  
   })
   describe('word has been guessed', () => {
